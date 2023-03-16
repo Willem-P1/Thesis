2beadf284dcf80ccc151aee5fcdf0859eba1a744
Willem-P1 <w.ploegstra@student.maastrichtuniversity.nl>
2023-03-16 17:20:38 +0100

    Added cherry reduction and vertex supression

--- code/Main.java
+++ code/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Parser l = new Parser("code/test.txt");
         Tree[] trees  = l.parse();
+
         System.out.println(trees[0]);
+        System.out.println(trees[1]);
+        // trees[0].supressDeg2Vertex();
+        trees[0].commonCherryReduction(trees[1]);
+        System.out.println(trees[0]);
+        System.out.println(trees[1]);
     }
 }
